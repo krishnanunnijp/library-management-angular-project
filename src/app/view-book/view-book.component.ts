a2cfb8435e87b4aa87af4d8fd3e73767f4dd595f
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 12:45:11 +0530

    edited

--- src/app/view-book/view-book.component.ts
+++ src/app/view-book/view-book.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-book.component.css']
 })
 export class ViewBookComponent {
+  data :any=[
+    {
+      "title":"Randam oozham",
+      "image": "https://m.media-amazon.com/images/I/41fRojGGDUL.jpg",
+      "description":"masterpiece of Jnanpith winning writer MT Vasudevan Nair. It was translated into English as Second Turn in 1997"
+      
+    },
+    {
+      "title":"Ponniyin selvan ",
+      "image": "https://rukminim1.flixcart.com/image/416/416/book/8/4/1/ponniyin-selvan-part-ii-original-imadeuhn6wa96dnw.jpeg?q=70",
+      "description":"The Story Is About The Historical Events, Queer Personalities And The Ruler Of Chola Dynasty Of Tamilnadu In 10th Century A.d"
+      
+    },
+    {
+      "title":"In Search of Lost Time ",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/7hqv6ddaqv363p4hadx6lymotow1",
+      "description":"Swann's Way, the first part of A la recherche de temps perdu, Marcel Proust's seven-part cycle, was published in 1913. "
+      
+    },
+    {
+      "title":"Ulysses",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/f7nkbyqfsnrrlct3hs01jkrz2vdi",
+      "description":"Ulysses chronicles the passage of Leopold Bloom through Dublin during an ordinary day, June 16, 1904 bulk of the card's content."
+      
+    },
+    {
+      "title":"Don Quixote ",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/bn8dmzg3y0fcofvl6azmbyl6bry4",
+      "description":"Alonso Quixano, a retired country gentleman in his fifties, lives in an unnamed section of La Mancha with his niece and a housekeeper. "
+      
+    },
+    {
+      "title":"One Hundred Years of Solitude",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/evtw45j15ooutm5tnz3ev1r9lk0e",
+      "description":"masterpiece of Jnanpith winning writer MT Vasudevan Nair. It was translated "
+      
+    },
+    {
+      "title":"Moby Dick ",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/epke1jolmtb858vv9ctbtxpa8yi6",
+      "description":"One of the 20th century's enduring works, One Hundred Years of Solitude is a widely beloved "
+      
+    },
+    {
+      "title":"The Great Gatsby",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/m8sdet02cc7mdl8l6jow8cpl7co2",
+      "description":"The novel chronicles an era that Fitzgerald himself dubbed the \"Jazz Age\". Following the shock and chaos of World War I"
+      
+    },
+    {
+      "title":"War and Peace",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/hwi2qqngyicqz7s7ps4x4k1ei14k",
+      "description":"First published in 1851, Melville's masterpiece is, in Elizabeth Hardwick's words, \"the greatest novel in American literature."
+      
+    },
+    {
+      "title":"Hamlet ",
+      "image": "https://d3i5mgdwi2ze58.cloudfront.net/94t9qfwmt3s3tbw24xhjquc1c7l5",
+      "description":"The Tragedy of Hamlet, Prince of Denmark, or more simply Hamlet, is a tragedy by William Shakespeare, believed to have been written"
+      
+    },
+  ]
 
 }
