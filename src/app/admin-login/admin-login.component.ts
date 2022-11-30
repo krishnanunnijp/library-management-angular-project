a61d2549f845f958186a3390f30fa920aef1d99d
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-30 10:47:48 +0530

    authentication part is completed

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 export class AdminLoginComponent {
   name=""
   password=""
+constructor(private route:Router){}
 
   adminlog=()=>
   {
@@ -16,9 +18,10 @@ export class AdminLoginComponent {
     "password":this.password
   }
   console.log(data)
+  
 
   if (this.name=="admin"  &&  this.password=="12345") {
-    alert("valid login")
+      this.route.navigate(['/add'])
     
   } else {
     alert("invalid credentials")
